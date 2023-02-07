8890f0feaa37e0fabb1e3b7fd7b1e7eeb904bd70
Abbas Haider <3057693@eeecs.qub.ac.uk>
2023-02-07 13:12:07 +0000

    Added Transform PIL to Tensor

--- common/datasetV3.py
+++ common/datasetV3.py
@@ -43,9 +43,7 @@ class TrainingData(LFWDataset):
 
 class EvaluationData(LFWDataset):
     def __init__(self, file, transform):
-        pdb.set_trace()
         super().__init__(file, transform)
-
     def __getitem__(self, index):
         return super().__getitem__(index)
     
