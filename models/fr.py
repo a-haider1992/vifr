e3d4a6510806edc8cf8791f942a06db2b3400e86
Abbas Haider <3057693@eeecs.qub.ac.uk>
2023-02-07 14:43:12 +0000

    Resolving dim mistmatch error

--- models/fr.py
+++ models/fr.py
@@ -32,19 +32,22 @@ class FR(BasicTask):
                 transforms.ToTensor(),
                 transforms.Normalize(mean=[0.5, ], std=[0.5, ])
             ])
-        
-        ## AIFR paper approach
+
+        # AIFR paper approach
         # train_dataset = TrainImageDataset(opt.dataset_name, self.train_transform)
 
-        ## VIFR approach
+        # VIFR approach
         # train_dataset = TrainDataset(opt.dataset_name, train_transform)
 
-        ## Transform for LFW
-        lfw_transform = transforms.Compose([transforms.ToTensor()])
-        ## LFW dataset
+        # Transform for LFW
+        lfw_transform = transforms.Compose(
+            [
+                transforms.ToTensor(),
+                transforms.Resize([opt.image_size, opt.image_size])
+            ])
+        # LFW dataset
         train_lfw_dataset = TrainingData('peopleDevTrain.csv', lfw_transform)
         test_lfw_dataset = EvaluationData('peopleDevTest.csv', lfw_transform)
-        
 
         evaluation_transform = transforms.Compose(
             [
@@ -54,9 +57,9 @@ class FR(BasicTask):
                 transforms.Normalize(mean=[0.5, ], std=[0.5, ])
             ])
 
-        ## AIFR paper approach
+        # AIFR paper approach
         # evaluation_dataset = EvaluationImageDataset(opt.evaluation_dataset, evaluation_transform)
-        ## VIFR approach
+        # VIFR approach
         # evaluation_dataset = EvaluationDataset(opt.evaluation_dataset, evaluation_transform)
 
         weights = None
@@ -74,7 +77,8 @@ class FR(BasicTask):
             train_lfw_dataset, batch_size=opt.batch_size, num_workers=opt.num_worker, drop_last=True
         )
 
-        evaluation_loader = torch.utils.data.DataLoader(test_lfw_dataset, num_workers=opt.num_worker)
+        evaluation_loader = torch.utils.data.DataLoader(
+            test_lfw_dataset, num_workers=opt.num_worker)
 
         # Train Prefetcher
         self.prefetcher = DataPrefetcher(train_loader)
@@ -88,20 +92,22 @@ class FR(BasicTask):
         head = CosFace(in_features=512, out_features=len(self.prefetcher.__loader__.dataset.classes),
                        s=opt.head_s, m=opt.head_m)
 
-        estimation_network = AgeEstimationModule(input_size=opt.image_size, age_group=opt.age_group)
+        estimation_network = AgeEstimationModule(
+            input_size=opt.image_size, age_group=opt.age_group)
 
-        da_discriminator = AgeEstimationModule(input_size=opt.image_size, age_group=opt.age_group)
+        da_discriminator = AgeEstimationModule(
+            input_size=opt.image_size, age_group=opt.age_group)
 
         if opt.gfr:
-            optimizer = torch.optim.SGD(list(backbone.parameters()) + \
-                                    list(head.parameters()), 
-                                    momentum=opt.momentum, lr=opt.learning_rate)
+            optimizer = torch.optim.SGD(list(backbone.parameters()) +
+                                        list(head.parameters()),
+                                        momentum=opt.momentum, lr=opt.learning_rate)
         else:
-            optimizer = torch.optim.SGD(list(backbone.parameters()) + \
-                                    list(head.parameters()) + \
-                                    list(estimation_network.parameters()) + \
-                                    list(da_discriminator.parameters()),
-                                    momentum=opt.momentum, lr=opt.learning_rate)
+            optimizer = torch.optim.SGD(list(backbone.parameters()) +
+                                        list(head.parameters()) +
+                                        list(estimation_network.parameters()) +
+                                        list(da_discriminator.parameters()),
+                                        momentum=opt.momentum, lr=opt.learning_rate)
 
         backbone, head, estimation_network, da_discriminator = convert_to_ddp(backbone, head, estimation_network,
                                                                               da_discriminator)
@@ -114,7 +120,8 @@ class FR(BasicTask):
         self.grl = GradientReverseLayer()
         self.scaler = scaler
 
-        self.logger.modules = [optimizer, backbone, head, estimation_network, da_discriminator, scaler]
+        self.logger.modules = [optimizer, backbone, head,
+                               estimation_network, da_discriminator, scaler]
         if opt.restore_iter > 0:
             self.logger.load_checkpoints(opt.restore_iter)
 
@@ -125,7 +132,8 @@ class FR(BasicTask):
         assert step > 0, 'batch index should large than 0'
         opt = self.opt
         if step > opt.warmup:
-            lr = opt.learning_rate * (opt.gamma ** np.sum(np.array(opt.milestone) < step))
+            lr = opt.learning_rate * \
+                (opt.gamma ** np.sum(np.array(opt.milestone) < step))
         else:
             lr = step * opt.learning_rate / opt.warmup
         lr = max(1e-4, lr)
@@ -135,7 +143,8 @@ class FR(BasicTask):
     def compute_age_loss(self, x_age, x_group, ages):
         opt = self.opt
         age_loss = F.mse_loss(get_dex_age(x_age), ages) + \
-                   F.cross_entropy(x_group, age2group(ages, age_group=opt.age_group).long())
+            F.cross_entropy(x_group, age2group(
+                ages, age_group=opt.age_group).long())
         return age_loss
 
     def forward_da(self, x_id, ages):
@@ -150,36 +159,37 @@ class FR(BasicTask):
         self.head.train()
 
         if opt.gfr:
-            ## For LFW type datasets
+            # For LFW type datasets
             images, labels = inputs
+            embedding = self.backbone(images)
         else:
-            ## For casia-webface type datasets
+            # For casia-webface type datasets
             images, labels, ages, genders = inputs
             self.da_discriminator.train()
             self.estimation_network.train()
-
-        if opt.amp:
-            with amp.autocast():
+            if opt.amp:
+                with amp.autocast():
+                    embedding, x_id, x_age = self.backbone(
+                        images, return_age=True)
+                embedding = embedding.float()
+                x_id = x_id.float()
+                x_age = x_age.float()
+            else:
                 embedding, x_id, x_age = self.backbone(images, return_age=True)
-            embedding = embedding.float()
-            x_id = x_id.float()
-            x_age = x_age.float()
-        else:
-            embedding, x_id, x_age = self.backbone(images, return_age=True)
 
         if opt.gfr:
-            ######## Train GFR only
+            # Train GFR only
             id_loss = F.cross_entropy(self.head(embedding, labels), labels)
             self.optimizer.zero_grad()
             self.optimizer.step()
             id_loss.backward()
-            apply_weight_decay(self.backbone, self.head, 
+            apply_weight_decay(self.backbone, self.head,
                                weight_decay_factor=opt.weight_decay, wo_bn=True)
             id_loss = reduce_loss(id_loss)
             lr = self.optimizer.param_groups[0]['lr']
             self.logger.msg([id_loss, lr], n_iter)
         else:
-            ######## Train Face Recognition with ages and genders
+            # Train Face Recognition with ages and genders
             id_loss = F.cross_entropy(self.head(embedding, labels), labels)
             x_age, x_group = self.estimation_network(x_age)
             age_loss = self.compute_age_loss(x_age, x_group, ages)
@@ -194,13 +204,14 @@ class FR(BasicTask):
             self.optimizer.zero_grad()
             total_loss.backward()
             apply_weight_decay(self.backbone, self.head, self.estimation_network,
-                            weight_decay_factor=opt.weight_decay, wo_bn=True)
+                               weight_decay_factor=opt.weight_decay, wo_bn=True)
             if opt.amp:
                 self.scaler.step(self.optimizer)
                 self.scaler.update()
             else:
                 self.optimizer.step()
 
-            id_loss, da_loss, age_loss = reduce_loss(id_loss, da_loss, age_loss)
+            id_loss, da_loss, age_loss = reduce_loss(
+                id_loss, da_loss, age_loss)
             lr = self.optimizer.param_groups[0]['lr']
             self.logger.msg([id_loss, da_loss, age_loss, lr], n_iter)
