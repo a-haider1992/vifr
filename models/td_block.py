971d98d9185f3d965fc204f04f5a4cb6d8c5d985
Abbas Haider <3057693@eeecs.qub.ac.uk>
2023-04-04 15:31:46 +0100

    Reformatted td block

--- models/td_block.py
+++ models/td_block.py
@@ -171,7 +171,7 @@ class MyViT(nn.Module):
 
         # 4) Transformer encoder blocks
         self.blocks = nn.ModuleList(
-            [self.MyViTBlock(hidden_d, n_heads) for _ in range(n_blocks)])
+            [MyViTBlock(hidden_d, n_heads) for _ in range(n_blocks)])
 
         # 5) Classification MLP
         self.mlp = nn.Sequential(
@@ -199,45 +199,45 @@ class MyViT(nn.Module):
 
         return self.mlp(out)
 
-    class MSA(nn.Module):
-        def __init__(self, d, n_heads=2) -> None:
-            super(self.MSA, self).__init__()
-            self.d = d
-            self.n_heads = n_heads
+class MSA(nn.Module):
+    def __init__(self, d, n_heads=2) -> None:
+        super(self.MSA, self).__init__()
+        self.d = d
+        self.n_heads = n_heads
 
-            assert d % n_heads == 0, f"Can't divide dimension {d} into {n_heads} heads"
-            d_head = int(d / n_heads)
-            self.q_mappings = nn.ModuleList(
-                [nn.Linear(d_head, d_head) for _ in range(self.n_heads)])
-            self.k_mappings = nn.ModuleList(
-                [nn.Linear(d_head, d_head) for _ in range(self.n_heads)])
-            self.v_mappings = nn.ModuleList(
-                [nn.Linear(d_head, d_head) for _ in range(self.n_heads)])
-            self.d_head = d_head
-            self.softmax = nn.Softmax(dim=-1)
+        assert d % n_heads == 0, f"Can't divide dimension {d} into {n_heads} heads"
+        d_head = int(d / n_heads)
+        self.q_mappings = nn.ModuleList(
+            [nn.Linear(d_head, d_head) for _ in range(self.n_heads)])
+        self.k_mappings = nn.ModuleList(
+            [nn.Linear(d_head, d_head) for _ in range(self.n_heads)])
+        self.v_mappings = nn.ModuleList(
+            [nn.Linear(d_head, d_head) for _ in range(self.n_heads)])
+        self.d_head = d_head
+        self.softmax = nn.Softmax(dim=-1)
 
-        def forward(self, sequences):
-            # Sequences has shape (N, seq_length, token_dim)
-            # We go into shape    (N, seq_length, n_heads, token_dim / n_heads)
-            # And come back to    (N, seq_length, item_dim)  (through concatenation)
-            result = []
-            for sequence in sequences:
-                seq_result = []
-                for head in range(self.n_heads):
-                    q_mapping = self.q_mappings[head]
-                    k_mapping = self.k_mappings[head]
-                    v_mapping = self.v_mappings[head]
+    def forward(self, sequences):
+        # Sequences has shape (N, seq_length, token_dim)
+        # We go into shape    (N, seq_length, n_heads, token_dim / n_heads)
+        # And come back to    (N, seq_length, item_dim)  (through concatenation)
+        result = []
+        for sequence in sequences:
+            seq_result = []
+            for head in range(self.n_heads):
+                q_mapping = self.q_mappings[head]
+                k_mapping = self.k_mappings[head]
+                v_mapping = self.v_mappings[head]
 
-                    seq = sequence[:, head *
-                                self.d_head: (head + 1) * self.d_head]
-                    q, k, v = q_mapping(seq), k_mapping(
-                        seq), v_mapping(seq)
+                seq = sequence[:, head *
+                            self.d_head: (head + 1) * self.d_head]
+                q, k, v = q_mapping(seq), k_mapping(
+                    seq), v_mapping(seq)
 
-                    attention = self.softmax(
-                        q @ k.T / (self.d_head ** 0.5))
-                    seq_result.append(attention @ v)
-                result.append(torch.hstack(seq_result))
-            return torch.cat([torch.unsqueeze(r, dim=0) for r in result])
+                attention = self.softmax(
+                    q @ k.T / (self.d_head ** 0.5))
+                seq_result.append(attention @ v)
+            result.append(torch.hstack(seq_result))
+        return torch.cat([torch.unsqueeze(r, dim=0) for r in result])
 
 class MyViTBlock(nn.Module):
     def __init__(self, hidden_d, n_heads, mlp_ratio=4):
