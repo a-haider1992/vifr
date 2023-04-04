9d34f1cb71772e57f77ef6e9a1d9efa9887d9984
Abbas Haider <3057693@eeecs.qub.ac.uk>
2023-04-04 15:27:15 +0100

    Reformatted TD block class

--- models/td_block.py
+++ models/td_block.py
@@ -86,7 +86,7 @@ class TDTask(BasicTask):
 
     def set_model(self):
         opt = self.opt
-        self.tdblock = self.MyViT((1, opt.image_size, opt.image_size), n_patches=8, n_blocks=2,
+        self.tdblock = MyViT((1, opt.image_size, opt.image_size), n_patches=8, n_blocks=2,
                                   hidden_d=8, n_heads=2, out_d=len(self.prefetcher.__loader__.dataset.classes))
         self.tdblock = convert_to_ddp(self.tdblock)
         self.optimizer = torch.optim.SGD(list(self.tdblock.parameters()),
@@ -123,138 +123,138 @@ class TDTask(BasicTask):
         lr = self.optimizer.param_groups[0]['lr']
         self.logger.msg([age_loss, lr], n_iter)
 
-    def patchify(images, n_patches):
-        n, c, w, h = images.shape
+def patchify(images, n_patches):
+    n, c, w, h = images.shape
 
-        assert h == w, "Patchify method works for square images only"
+    assert h == w, "Patchify method works for square images only"
 
-        patches = torch.zeros(n, n_patches ** 2, h * w // n_patches ** 2)
-        patch_size = h // n_patches
+    patches = torch.zeros(n, n_patches ** 2, h * w // n_patches ** 2)
+    patch_size = h // n_patches
 
-        for idx, image in enumerate(images):
-            for i in range(n_patches):
-                for j in range(n_patches):
-                    patch = image[:, i * patch_size: (i + 1) * patch_size,
-                                  j * patch_size: (j + 1) * patch_size]
-                    patches[idx, i * n_patches + j] = patch.flatten()
-        return patches
+    for idx, image in enumerate(images):
+        for i in range(n_patches):
+            for j in range(n_patches):
+                patch = image[:, i * patch_size: (i + 1) * patch_size,
+                            j * patch_size: (j + 1) * patch_size]
+                patches[idx, i * n_patches + j] = patch.flatten()
+    return patches
 
-    def get_positional_embeddings(sequence_length, d):
-        result = torch.ones(sequence_length, d)
-        for i in range(sequence_length):
-            for j in range(d):
-                result[i][j] = np.sin(
-                    i / (10000 ** (j/d))) if j % 2 == 0 else np.cos(i / (10000 ** ((j - 1) / d)))
-        return result
+def get_positional_embeddings(sequence_length, d):
+    result = torch.ones(sequence_length, d)
+    for i in range(sequence_length):
+        for j in range(d):
+            result[i][j] = np.sin(
+                i / (10000 ** (j/d))) if j % 2 == 0 else np.cos(i / (10000 ** ((j - 1) / d)))
+    return result
 
-    class MyViT(nn.Module):
-        def __init__(self, chw, n_patches, n_blocks, hidden_d, n_heads, out_d) -> None:
-            super().__init__()
+class MyViT(nn.Module):
+    def __init__(self, chw, n_patches, n_blocks, hidden_d, n_heads, out_d) -> None:
+        super().__init__()
 
-            self.chw = chw
-            self.n_patches = n_patches
-            self.hidden_d = hidden_d
+        self.chw = chw
+        self.n_patches = n_patches
+        self.hidden_d = hidden_d
 
-            assert chw[1] % n_patches == 0, "Input shape not completely divisible by number of patches"
-            assert chw[2] % n_patches == 0, "Input shape not completely divisible by number of patches"
-            self.patch_size = (chw[1] / n_patches, chw[2] / n_patches)
+        assert chw[1] % n_patches == 0, "Input shape not completely divisible by number of patches"
+        assert chw[2] % n_patches == 0, "Input shape not completely divisible by number of patches"
+        self.patch_size = (chw[1] / n_patches, chw[2] / n_patches)
 
-            self.input_d = int(
-                chw[0] * self.patch_size[0] * self.patch_size[1])
-            self.linear_mapper = nn.Linear(self.input_d, self.hidden_d)
+        self.input_d = int(
+            chw[0] * self.patch_size[0] * self.patch_size[1])
+        self.linear_mapper = nn.Linear(self.input_d, self.hidden_d)
 
-            self.class_token = nn.Parameter(torch.rand(1, self.hidden_d))
+        self.class_token = nn.Parameter(torch.rand(1, self.hidden_d))
 
-            # 3) Positional embedding
-            self.register_buffer('positional_embeddings', self.get_positional_embeddings(
-                n_patches ** 2 + 1, hidden_d), persistent=False)
+        # 3) Positional embedding
+        self.register_buffer('positional_embeddings', get_positional_embeddings(
+            n_patches ** 2 + 1, hidden_d), persistent=False)
 
-            # 4) Transformer encoder blocks
-            self.blocks = nn.ModuleList(
-                [self.MyViTBlock(hidden_d, n_heads) for _ in range(n_blocks)])
+        # 4) Transformer encoder blocks
+        self.blocks = nn.ModuleList(
+            [self.MyViTBlock(hidden_d, n_heads) for _ in range(n_blocks)])
 
-            # 5) Classification MLP
-            self.mlp = nn.Sequential(
-                nn.Linear(self.hidden_d, out_d),
-                nn.Softmax(dim=-1)
-            )
+        # 5) Classification MLP
+        self.mlp = nn.Sequential(
+            nn.Linear(self.hidden_d, out_d),
+            nn.Softmax(dim=-1)
+        )
 
-        def forward(self, images):
-            n, c, h, w = images.shape
-            patches = self.patchify(images, self.n_patches)
-            tokens = self.linear_mapper(patches)
-            # Adding classification token to the tokens
-            tokens = torch.cat(
-                (self.class_token.expand(n, 1, -1), tokens), dim=1)
+    def forward(self, images):
+        n, c, h, w = images.shape
+        patches = patchify(images, self.n_patches)
+        tokens = self.linear_mapper(patches)
+        # Adding classification token to the tokens
+        tokens = torch.cat(
+            (self.class_token.expand(n, 1, -1), tokens), dim=1)
 
-            # Adding positional embedding
-            out = tokens + self.positional_embeddings.repeat(n, 1, 1)
+        # Adding positional embedding
+        out = tokens + self.positional_embeddings.repeat(n, 1, 1)
 
-            # Transformer Blocks
-            for block in self.blocks:
-                out = block(out)
+        # Transformer Blocks
+        for block in self.blocks:
+            out = block(out)
 
-            # Getting the classification token only
-            out = out[:, 0]
+        # Getting the classification token only
+        out = out[:, 0]
 
-            return self.mlp(out)
+        return self.mlp(out)
 
-        class MSA(nn.Module):
-            def __init__(self, d, n_heads=2) -> None:
-                super(self.MSA, self).__init__()
-                self.d = d
-                self.n_heads = n_heads
-
-                assert d % n_heads == 0, f"Can't divide dimension {d} into {n_heads} heads"
-                d_head = int(d / n_heads)
-                self.q_mappings = nn.ModuleList(
-                    [nn.Linear(d_head, d_head) for _ in range(self.n_heads)])
-                self.k_mappings = nn.ModuleList(
-                    [nn.Linear(d_head, d_head) for _ in range(self.n_heads)])
-                self.v_mappings = nn.ModuleList(
-                    [nn.Linear(d_head, d_head) for _ in range(self.n_heads)])
-                self.d_head = d_head
-                self.softmax = nn.Softmax(dim=-1)
-
-            def forward(self, sequences):
-                # Sequences has shape (N, seq_length, token_dim)
-                # We go into shape    (N, seq_length, n_heads, token_dim / n_heads)
-                # And come back to    (N, seq_length, item_dim)  (through concatenation)
-                result = []
-                for sequence in sequences:
-                    seq_result = []
-                    for head in range(self.n_heads):
-                        q_mapping = self.q_mappings[head]
-                        k_mapping = self.k_mappings[head]
-                        v_mapping = self.v_mappings[head]
-
-                        seq = sequence[:, head *
-                                       self.d_head: (head + 1) * self.d_head]
-                        q, k, v = q_mapping(seq), k_mapping(
-                            seq), v_mapping(seq)
-
-                        attention = self.softmax(
-                            q @ k.T / (self.d_head ** 0.5))
-                        seq_result.append(attention @ v)
-                    result.append(torch.hstack(seq_result))
-                return torch.cat([torch.unsqueeze(r, dim=0) for r in result])
-
-    class MyViTBlock(nn.Module):
-        def __init__(self, hidden_d, n_heads, mlp_ratio=4):
-            super(self.MyViTBlock, self).__init__()
-            self.hidden_d = hidden_d
+    class MSA(nn.Module):
+        def __init__(self, d, n_heads=2) -> None:
+            super(self.MSA, self).__init__()
+            self.d = d
             self.n_heads = n_heads
 
-            self.norm1 = nn.LayerNorm(hidden_d)
-            self.mhsa = self.MSA(hidden_d, n_heads)
-            self.norm2 = nn.LayerNorm(hidden_d)
-            self.mlp = nn.Sequential(
-                nn.Linear(hidden_d, mlp_ratio * hidden_d),
-                nn.GELU(),
-                nn.Linear(mlp_ratio * hidden_d, hidden_d)
-            )
+            assert d % n_heads == 0, f"Can't divide dimension {d} into {n_heads} heads"
+            d_head = int(d / n_heads)
+            self.q_mappings = nn.ModuleList(
+                [nn.Linear(d_head, d_head) for _ in range(self.n_heads)])
+            self.k_mappings = nn.ModuleList(
+                [nn.Linear(d_head, d_head) for _ in range(self.n_heads)])
+            self.v_mappings = nn.ModuleList(
+                [nn.Linear(d_head, d_head) for _ in range(self.n_heads)])
+            self.d_head = d_head
+            self.softmax = nn.Softmax(dim=-1)
 
-        def forward(self, x):
-            out = x + self.mhsa(self.norm1(x))
-            out = out + self.mlp(self.norm2(out))
-            return out
+        def forward(self, sequences):
+            # Sequences has shape (N, seq_length, token_dim)
+            # We go into shape    (N, seq_length, n_heads, token_dim / n_heads)
+            # And come back to    (N, seq_length, item_dim)  (through concatenation)
+            result = []
+            for sequence in sequences:
+                seq_result = []
+                for head in range(self.n_heads):
+                    q_mapping = self.q_mappings[head]
+                    k_mapping = self.k_mappings[head]
+                    v_mapping = self.v_mappings[head]
+
+                    seq = sequence[:, head *
+                                self.d_head: (head + 1) * self.d_head]
+                    q, k, v = q_mapping(seq), k_mapping(
+                        seq), v_mapping(seq)
+
+                    attention = self.softmax(
+                        q @ k.T / (self.d_head ** 0.5))
+                    seq_result.append(attention @ v)
+                result.append(torch.hstack(seq_result))
+            return torch.cat([torch.unsqueeze(r, dim=0) for r in result])
+
+class MyViTBlock(nn.Module):
+    def __init__(self, hidden_d, n_heads, mlp_ratio=4):
+        super(self.MyViTBlock, self).__init__()
+        self.hidden_d = hidden_d
+        self.n_heads = n_heads
+
+        self.norm1 = nn.LayerNorm(hidden_d)
+        self.mhsa = self.MSA(hidden_d, n_heads)
+        self.norm2 = nn.LayerNorm(hidden_d)
+        self.mlp = nn.Sequential(
+            nn.Linear(hidden_d, mlp_ratio * hidden_d),
+            nn.GELU(),
+            nn.Linear(mlp_ratio * hidden_d, hidden_d)
+        )
+
+    def forward(self, x):
+        out = x + self.mhsa(self.norm1(x))
+        out = out + self.mlp(self.norm2(out))
+        return out
