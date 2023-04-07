6a456b248d4defb3a9de5b0650b796bee9b234bd
Abbas Haider <3057693@eeecs.qub.ac.uk>
2023-04-07 10:16:10 +0100

    Changed paths of model save

--- models/mtlface.py
+++ models/mtlface.py
@@ -186,8 +186,10 @@ class MTLFace(object):
         opt = self.opt
         if opt.id_pretrained_path is None and dist.get_rank() == 0:
             root = os.path.dirname(__file__)
-            PATH = os.path.join(root, opt.model_save)
-            torch.save(self.fr.backbone.state_dict(), PATH)
+            PATH_BACKBONE = os.path.join(root, 'backbone.pt')
+            PATH_AGE_ESTIMATION = os.path.join(root, 'age_estimation_model.pt')
+            torch.save(self.fr.backbone.state_dict(), PATH_BACKBONE)
+            torch.save(self.fr.estimation_network.state_dict(), PATH_AGE_ESTIMATION)
 
     def isSame(self, embed1, embed2):
         result = torch.eq(embed1, embed2)
