0ceb3512630d1cc0f438b5a5b7104f10dece0720
Abbas Haider <3057693@eeecs.qub.ac.uk>
2023-04-09 17:24:45 +0100

    check GPU mem allocation of dataloader

--- models/fr.py
+++ models/fr.py
@@ -95,6 +95,10 @@ class FR(BasicTask):
                                                        batch_size=opt.batch_size,
                                                        sampler=sampler, pin_memory=True, num_workers=opt.num_worker,
                                                        drop_last=True)
+            print(f"GPU memory allocated before loading data: {torch.cuda.memory_allocated()} bytes")
+            for i, batch in enumerate(train_loader):
+                print(f"GPU memory allocated after loading batch {i}: {torch.cuda.memory_allocated()} bytes")
+                
         else:
             return Exception("Database doesn't exist.")
 
