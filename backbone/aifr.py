45bc44e14192956bcc02c82294781d1904d00c12
Abbas Haider <3057693@eeecs.qub.ac.uk>
2023-04-26 09:56:07 +0100

    resolving err

--- backbone/aifr.py
+++ backbone/aifr.py
@@ -151,7 +151,7 @@ class AIResNet(IResNet):
     def __init__(self, input_size, num_layers, mode='ir', **kwargs):
         super(AIResNet, self).__init__(input_size, num_layers, mode)
         self.fsm = AttentionModule()
-        # self.channel_reducer = torch.nn.Conv2d(in_channels=512+256+128+64, out_channels=512, kernel_size=1)
+        self.channel_reducer = torch.nn.Conv2d(in_channels=512+256+128+64, out_channels=512, kernel_size=1)
         self.output_layer = nn.Sequential(
             nn.BatchNorm2d(512),
             nn.Dropout(),
