5e5d8dfcac310e7f19a8baf70afe726ff0400b53
Abbas Haider <3057693@eeecs.qub.ac.uk>
2023-05-10 08:44:49 +0100

    visual sota

--- models/mtlface.py
+++ models/mtlface.py
@@ -307,7 +307,7 @@ class MTLFace(object):
                 # embedding2, x_id2, x_age2, x_residual2 = self.fr.backbone(
                 #     image2, return_residual=True)
                 embedding2, x_id2, x_age2 = self.fr.backbone(
-                    image2, return_age=True)
+                    image1, return_age=True)
                 x1, x2, x3, x4, x5, _, _ = self.fr.backbone(
                     image1, return_shortcuts=True)
                 # embedding1 = self.fr.backbone(image1)
@@ -342,8 +342,8 @@ class MTLFace(object):
             ])
             x = x_id2
             for i in range(x.shape[1] // 3):
-                # if i > 5:
-                #     break
+                if i > 5:
+                    break
                 start_channel = i * 3
                 end_channel = start_channel + 3
                 img = transform(x[:, start_channel:end_channel, :, :].squeeze())
